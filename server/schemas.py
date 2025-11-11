becd94dd59c15f218fa012bd82c435ea25b16e9b
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2025-11-11 16:44:56 +0800

    Add schemas.py file

---
+++ server/schemas.py
@@ -0,0 +1,3 @@
+from app import ma 
+from models import User, Member, Group, GroupMember, Sermon, Event, EventRegistration, Donation, PrayerRequest, Page, Announcement, Media, ContactMessage, NavigationItem, NavigationMenu
+
